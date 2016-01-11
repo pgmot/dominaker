b3a4a18f710872b1b7021dc2bc761ac08616a328
mot <mot.contact.address.pc@gmail.com>
2016-01-12 00:23:59 +0900

    Register APIを叩くようにした

--- app/src/main/java/com/pgmot/dominaker/activity/IkaActivity.java
+++ app/src/main/java/com/pgmot/dominaker/activity/IkaActivity.java
@@ -7,20 +7,30 @@ import android.util.Log;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.gson.Gson;
 import com.neovisionaries.ws.client.WebSocket;
 import com.neovisionaries.ws.client.WebSocketAdapter;
 import com.neovisionaries.ws.client.WebSocketException;
 import com.neovisionaries.ws.client.WebSocketFactory;
-import com.pgmot.dominaker.gameobject.Ika;
+import com.neovisionaries.ws.client.WebSocketFrame;
 import com.pgmot.dominaker.R;
 import com.pgmot.dominaker.controller.LocationController;
 import com.pgmot.dominaker.controller.MapsController;
+import com.pgmot.dominaker.gameobject.Ika;
 import com.pgmot.dominaker.util.Util;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class IkaActivity extends FragmentActivity implements OnMapReadyCallback, LocationController.LocationControllerListener {
 
@@ -28,11 +38,15 @@ public class IkaActivity extends FragmentActivity implements OnMapReadyCallback,
     private WebSocket websocket;
     private boolean isWebsocketConnected = false;
     private String uuid;
+    private int teamID = -1;
     private String myMarkerUUID = "";
     private ConcurrentHashMap<String, Ika> ikaConcurrentHashMap = new ConcurrentHashMap<>();
     private ArrayList<Ika> ikas = new ArrayList<>();
     private LocationController locationController;
     private MapsController mapsController;
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient httpClient;
+    private static final String SERVER_URL = "dominaker-staging.herokuapp.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,22 +60,83 @@ public class IkaActivity extends FragmentActivity implements OnMapReadyCallback,
         locationController = new LocationController(this);
         locationController.addListener(this);
 
+        httpClient = new OkHttpClient();
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-
-
+                teamID = register(uuid);
                 connectWebsocket();
             }
         }).start();
     }
 
-    private void connectWebsocket() {
-        String url = "ws://192.168.5.72:4567";
+    private int register(String uuid) {
+        Gson gson = new Gson();
+
+        class RegisterRequestClass {
+            public String uuid;
+
+            public RegisterRequestClass(String uuid) {
+                this.uuid = uuid;
+            }
+        }
+
+        RequestBody requestBody = RequestBody.create(JSON, "{\"uuid\":\"" + uuid + "\"}");
+        Request request = new Request.Builder()
+                .url("https://" + SERVER_URL + "/register")
+                .post(requestBody)
+                .build();
+
         try {
-            websocket = new WebSocketFactory().createSocket(url);
-            websocket.connect();
+            Response response = httpClient.newCall(request).execute();
+            String responseString = response.body().string();
+            return gson.fromJson(responseString, RegisterResponseClass.class).team_id;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    static class RegisterResponseClass {
+        public int team_id;
+
+        public RegisterResponseClass(int team_id) {
+            this.team_id = team_id;
+        }
+    }
+
+    private void connectWebsocket() {
+        try {
+            websocket = new WebSocketFactory().createSocket("wss://" + SERVER_URL);
             websocket.addListener(new WebSocketAdapter() {
+                @Override
+                public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
+                    super.onConnected(websocket, headers);
+                    Log.v(LOG_TAG, "onConnected");
+
+                    isWebsocketConnected = true;
+                }
+
+                @Override
+                public void onConnectError(WebSocket websocket, WebSocketException exception) throws Exception {
+                    super.onConnectError(websocket, exception);
+                    Log.v(LOG_TAG, "onConnectError");
+
+                    isWebsocketConnected = false;
+                }
+
+                @Override
+                public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
+                    super.onDisconnected(websocket, serverCloseFrame, clientCloseFrame, closedByServer);
+                    Log.v(LOG_TAG, "onDisconnected");
+
+                    // 再接続処理
+                    websocket.recreate().connect();
+                    isWebsocketConnected = false;
+                }
+
                 @Override
                 public void onTextMessage(WebSocket websocket, String message) throws Exception {
                     String[] split = message.split(",");
@@ -78,12 +153,11 @@ public class IkaActivity extends FragmentActivity implements OnMapReadyCallback,
                 }
             });
 
-            isWebsocketConnected = true;
+            websocket.connect();
         } catch (IOException | WebSocketException e) {
             e.printStackTrace();
         }
     }
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
